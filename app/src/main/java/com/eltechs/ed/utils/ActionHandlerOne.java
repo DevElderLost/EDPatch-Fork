d43380a5c3d3431bdee9c4c45c56fc41165d5943
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-03-29 14:42:10 +0800

    Add files via upload

--- app/src/main/java/com/eltechs/ed/utils/ActionHandlerOne.java
+++ app/src/main/java/com/eltechs/ed/utils/ActionHandlerOne.java
@@ -1,1 +1,57 @@
-a
+package com.eltechs.ed.utils;
+
+import android.content.Context;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActionHandlerOne {
+    public static String getExternalStorageId(Context context) {
+        if (!isExternalStorageAvailable()) {
+            return (String) null;
+        }
+        File externalStorageDirectory = getExternalStorageDirectory(context);
+        if (externalStorageDirectory == null) {
+            return (String) null;
+        }
+        String[] split = externalStorageDirectory.getAbsolutePath().split("/");
+        return split.length > 2 ? split[2] : (String) null;
+    }
+
+    private static void addFoldersToList(File file, List<String> list) {
+        if (file != null && file.isDirectory()) {
+            File[] listFiles = file.listFiles();
+            if (listFiles != null) {
+                for (File file2 : listFiles) {
+                    if (file2.isDirectory()) {
+                        list.add(file2.getName());
+                    }
+                }
+            }
+        }
+    }
+
+    public static List<String> getFolderList(File file) {
+        ArrayList arrayList = new ArrayList();
+        addFoldersToList(file, arrayList);
+        if (arrayList.isEmpty()) {
+            File file2 = new File(file, "ExaGear");
+            if (!file2.exists()) {
+                file2.mkdir();
+            }
+            arrayList.add(file2.getName());
+        }
+        return arrayList;
+    }
+
+    private static File getExternalStorageDirectory(Context context) {
+        File[] externalFilesDirs = ContextCompat.getExternalFilesDirs(context, (String) null);
+        return (externalFilesDirs.length <= 1 || externalFilesDirs[1] == null) ? (File) null : externalFilesDirs[1];
+    }
+
+    public static boolean isExternalStorageAvailable() {
+        return Environment.getExternalStorageState().equals("mounted");
+    }
+}
