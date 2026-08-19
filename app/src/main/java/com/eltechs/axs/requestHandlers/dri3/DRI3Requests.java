b0bd6d255b564f34e757b2ee6ee76a5a754f3fe4
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-08-19 10:07:48 +0800

    feat: dri3 impl

--- app/src/main/java/com/eltechs/axs/requestHandlers/dri3/DRI3Requests.java
+++ app/src/main/java/com/eltechs/axs/requestHandlers/dri3/DRI3Requests.java
@@ -35,7 +35,7 @@ public final class DRI3Requests extends HandlerObjectBase {
     }
 
     private void handleQueryVersion(XClient client, int seq, byte minorOpcode,
-                                     XRequest request, XResponse response) {
+                                     XRequest request, XResponse response) throws java.io.IOException {
         request.readInt();
         request.readInt();
         response.sendSuccessReply((byte) 1, Version.MAJOR, Version.MINOR);
@@ -55,7 +55,7 @@ public final class DRI3Requests extends HandlerObjectBase {
     }
 
     private void handlePixmapFromBuffer(XClient client, int seq, byte minorOpcode,
-                                         XRequest request, XResponse response) {
+                                         XRequest request, XResponse response) throws java.io.IOException {
         int newPixmapId = request.readInt();
         int drawable = request.readInt();
         request.readInt(); // size
@@ -79,7 +79,7 @@ public final class DRI3Requests extends HandlerObjectBase {
     }
 
     private void handleBufferFromPixmap(XClient client, int seq, byte minorOpcode,
-                                         XRequest request, XResponse response) {
+                                         XRequest request, XResponse response) throws java.io.IOException {
         int pixmap = request.readInt();
         Dri3BufferAllocator.BufferInfo info = bufferAllocator.exportPixmapAsFd(pixmap);
         if (info == null) {
